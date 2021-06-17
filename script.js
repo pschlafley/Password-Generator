93888457b53862c095711975d02a18db4eee2a26
Peyton Schlafley <pschlafley0@gmail.com>
2021-06-17 16:54:20 -0500

    ffinished the password generator so that based off of the users criteria a random password will be generated for them

--- script.js
+++ script.js
@@ -1,36 +1,56 @@
- // var generateBtn = document.getElementById(generate);
+// var generateBtn = document.getElementById(generate);
 
-  //generateBtn.addEventListener("click", generatePassword);
+//generateBtn.addEventListener("click", generatePassword);
 
 // create a function that asks each question and prompts the user to confirm 
 
 
-var passWord= "";
-var upperCase =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdevghijklmnopqrstuv";
-var special = "~!@#$%^&*() _+=?><";
+var special = "~!@#$%^&*()_+=?><";
 var numbers = "1234567890";
 
-var length = Number(prompt("How many characters do you want your password to be?"));
-if (length >= 8 && length <= 100) {
-    generatePassword();
-}
-else {
-    window.alert("Must be 8 or more!");
-}
+const generateBtn = document.querySelector('#generate');
+const generatedPass = document.querySelector('#password');
 
 function generatePassword() {
+    let possibleChars = "";
+    let result = "";
 
-    window.confirm("Do you want uppercase characters? If yes click the OK button"); 
-    while (passWord.length >= 8 || passWord.length < 70) {
-        passWord += upperCase[Math.floor(Math.random()) * upperCase];
+    let passwordLength = window.prompt("How many characters do you want in your password?");
+
+    if (passwordLength < 8 || passwordLength > 20) {
+        window.alert('Password must be between 8 and 15! Hit the Generate Password Button again!');
+    };
+
+    let upperConfirm = window.confirm('Do you want UpperCase Characters?');
+    let lowerConfirm = window.confirm('Do you want lowerCase Characters?');
+    let specialConfirm = window.confirm('Do you want special Charaters?');
+    let numbersConfirm = window.confirm('Do you wan Numbers?');
+
+    if (upperConfirm) {
+        possibleChars += upperCase;
+    };
+
+    if (lowerConfirm) {
+        possibleChars += lowerCase;
+    };
+
+    if (specialConfirm) {
+        possibleChars += special;
+    };
+
+    if (numbersConfirm) {
+        possibleChars += numbers;
+    };
+
+    for (let i = 0; i < passwordLength; i++) {
+        result += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
     }
-};    
-generatePassword();
 
-      
-
+    generatedPass.textContent = result;
+};
 
 
 
-
+generateBtn.addEventListener('click', generatePassword);
